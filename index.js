30389777b9ff0dfc91c7941a82d80a28a6dd9ec7
jaun <jawoon816@gmail.com>
2022-11-08 19:19:01 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// 1.세 수 중 최솟값
+// function solution(a, b, c){
+//   if(a<b && a<c) {
+//     return a;
+//   } else if (b<a && b<c){
+//     return b;
+//   } else if (c<a && c<b) {
+//     return c;
+//   }
+// } 
+// function solution(a, b, c){
+//   let answer;
+//   if(a<b) answer =a;
+//   else answer=b;
+//   if(c<answer) answer = c;
+//   return answer;
+// } 
+
+// console.log(solution(6, 5, 11));
+
+// 2.삼각형 판별하기
+function solution(a, b, c){
+  let answer="YES", max;
+ 
+  return answer;
+}
+
+console.log(solution(13, 33, 17));
