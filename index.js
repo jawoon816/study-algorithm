aa08336822c51649b093a658d3121263e33c5c4a
jaun <jawoon816@gmail.com>
2022-11-08 20:27:27 +0900

    문제3. 연필개수

--- index.js
+++ index.js
@@ -32,14 +32,23 @@
 //   }
 // }
 
-function solution(a, b, c){
-  let answer="YES", max;
-  let sum = a+b+c;
-  if(a>b) max = a;
-  else max = b;
-  if(c>max) max = c;
-  if(sum-max<=max) answer = 'NO';
+// function solution(a, b, c){
+//   let answer="YES", max;
+//   let sum = a+b+c;
+//   if(a>b) max = a;
+//   else max = b;
+//   if(c>max) max = c;
+//   if(sum-max<=max) answer = 'NO';
+//   return answer;
+// }
+
+// console.log(solution(13, 33, 17));
+
+// 3.연필개수
+function solution(n){
+  let answer;
+  answer=Math.ceil(n/12);
   return answer;
 }
 
-console.log(solution(13, 33, 17));
+console.log(solution(178));
