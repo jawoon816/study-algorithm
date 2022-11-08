f7826cb36a9e1cfe3035fe68654cc3bd983d812b
jaun <jawoon816@gmail.com>
2022-11-08 19:51:27 +0900

    문제2

--- index.js
+++ index.js
@@ -19,9 +19,26 @@
 // console.log(solution(6, 5, 11));
 
 // 2.삼각형 판별하기
+// 가장 긴 한 변의 길이 < 나머지 변의 길이의 합
+// function solution(a, b, c){
+//   if(a>b && a>c && a<c+b) {
+//     return 'YES'
+//   } else if (b>a && b>c && b<a+c) {
+//     return 'YES'
+//   } else if (c>a && c>b && c<a+b) {
+//     return  'YES'
+//   } else {
+//     return "NO"
+//   }
+// }
+
 function solution(a, b, c){
   let answer="YES", max;
- 
+  let sum = a+b+c;
+  if(a>b) max = a;
+  else max = b;
+  if(c>max) max = c;
+  if(sum-max<=max) answer = 'NO';
   return answer;
 }
 
