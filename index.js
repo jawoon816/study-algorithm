773a9f03d59b748a8e4fb4eb53e114cf3dffd6d3
jaun <jawoon816@gmail.com>
2022-11-10 08:26:54 +0900

    10.문자찾기

--- index.js
+++ index.js
@@ -197,3 +197,22 @@
 // let str="BANANA";
 // console.log(solution(str));
 
+// 10.문자찾기
+// function solution(s, t){
+//   let answer = 0;
+//   for(let x of s){
+//     if(x===t){
+//       answer++
+//     }
+//   }
+//   return answer;
+// }
+
+function solution(s, t){
+  let answer = s.split(t).length-1
+  return answer;
+}
+
+let str="COMPUTERPROGRAMMING";
+console.log(solution(str, 'G'));
+
