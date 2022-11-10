bf2c47e20df0bd92a2544d0ff4f0ee69776a9e6b
jaun <jawoon816@gmail.com>
2022-11-10 09:19:18 +0900

    13.대소문자 변환

--- index.js
+++ index.js
@@ -273,5 +273,18 @@
 // let str='ItisTimeToStudy'
 // console.log(solution(str))
 
+// 13.대소문자변환
+function solution(s){  
+  let answer="";
+  for(let x of s){
+      if(x===x.toUpperCase()) answer+=x.toLowerCase();
+      else answer+=x.toUpperCase();
+  }
+  return answer;
+}
+
+console.log(solution("StuDY"));
+
+
 
 
