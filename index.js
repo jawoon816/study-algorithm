e49abd047e89ba078c1ee9ea0c6625b9bb6c271d
jaun <jawoon816@gmail.com>
2022-11-12 22:57:21 +0900

    16.중복문자 제거

--- index.js
+++ index.js
@@ -327,6 +327,25 @@
 // }
 // console.log(solution("study"));
 
+// 16.중복문자 제거
+function solution(s){  
+  let answer="";
+  
+
+  return answer;
+}
+
+// function solution(s){  
+//   let answer="";
+//   //console.log(s.indexOf("K"));
+//   for(let i=0; i<s.length; i++){
+//       //console.log(s[i], i, s.indexOf(s[i]));
+//       if(s.indexOf(s[i])===i) answer+=s[i];
+//   }
+//   return answer;
+// }
+console.log(solution("ksekkset"));
+
 
 
 
