f5f1add6256fd064f82f59d160ee2f1d138535c9
jaun <jawoon816@gmail.com>
2022-11-14 23:36:50 +0900

    17.중복단어 제거 문제풀기

--- index.js
+++ index.js
@@ -339,6 +339,20 @@
 // }
 // console.log(solution("ksekkset"));
 
+// 17.중복단어 제거
+function solution(s){  
+  let answer=[]
+  for(let i=0; i<s.length; i++) {
+    // console.log(i)
+    if(s.indexOf(s[i])===i){
+      answer.push(s[i])
+    }
+  }
+  return answer;
+}
+let str=["good", "time", "good", "time", "student"];
+console.log(solution(str));
+
 
 
 
