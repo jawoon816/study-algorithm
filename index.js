781d3fd3a6bebf0f7fa548de2e6854733e56acb2
jaun <jawoon816@gmail.com>
2022-11-24 09:02:30 +0900

    2-4 for문 수정

--- index.js
+++ index.js
@@ -447,8 +447,22 @@ function solution(arr) {
   return answer;
 }
 
+function solution(arr) {
+  let answer = 0,
+    cnt = 0;
+  for (let x of arr) {
+    if (x === 1) {
+      cnt++;
+      answer += cnt;
+    } else cnt = 0;
+  }
+  return answer;
+}
+
 let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
 console.log(solution(arr));
+
+// 5.등수구하기
 
 
 
