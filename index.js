26202086347c7170d0a64021d7d3c31735e51916
jaun <jawoon816@gmail.com>
2022-11-24 09:01:37 +0900

    2-4.점수계산

--- index.js
+++ index.js
@@ -414,25 +414,41 @@
 // console.log(solution(arr));
 
 // 3.가위바위보
-function solution(a, b) {
-  let answer = "";
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] === b[i]) answer += "D";
-    else if (
-      (a[i] === 1 && b[i] === 3) ||
-      (a[i] === 2 && b[i] === 1) ||
-      (a[i] === 3 && b[i] === 1)
-    )
-      answer += "A";
-    else answer += "B";
-  }
+// function solution(a, b) {
+//   let answer = "";
+//   for (let i = 0; i < a.length; i++) {
+//     if (a[i] === b[i]) answer += "D";
+//     else if (
+//       (a[i] === 1 && b[i] === 3) ||
+//       (a[i] === 2 && b[i] === 1) ||
+//       (a[i] === 3 && b[i] === 1)
+//     )
+//       answer += "A";
+//     else answer += "B";
+//   }
 
+//   return answer;
+// }
+
+// let a = [2, 3, 3, 1, 3];
+// let b = [1, 1, 2, 2, 3];
+// console.log(solution(a, b));
+
+// 4. 점수 계산 
+function solution(arr) {
+  let answer = 0,
+    cnt = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 1) {
+      cnt++;
+      answer += cnt;
+    } else cnt = 0;
+  }
   return answer;
 }
 
-let a = [2, 3, 3, 1, 3];
-let b = [1, 1, 2, 2, 3];
-console.log(solution(a, b));
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
 
 
 
